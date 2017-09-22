e740a625b6f50311bfa5fb33f8e5ce7f25051725
isLishude <lishude@aliyun.com>
2017-09-22 10:50:14 +0800

    test init

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+/**
+ * get a radom string or number of given range
+ * @author islishude
+ * @license MIT
+ * @version 1.0.0
+ */
+export default class Random {
+    /**
+     * get a number of given range
+     * @param min {number} the min number
+     * @param max {number} the max number
+     * @return {number} a radom number
+     */
+    getNum(min: number, max: number) {
+        // if the params are not number,reports error
+        if (typeof min !== 'number' || typeof max !== 'number')
+            throw new Error('param invalidate,should be a number')
+        // if the order is not incorrect,exchange param position
+        if (min > max) {
+            [min, max] = [max, min]
+        }
+        // if min param is equal with max param,return min
+        if (min === max) {
+            return min
+        }
+        return Math.floor(Math.random() * (max - min + 1)) + min
+    }
+    /**
+     * get a radom string
+     * @param length {number} default is 6
+     * @return <string> a radom string,default length is 6
+     */
+    getStr(length: number = 6) {
+        let str: string = 'abcdefghijklmnopqrstuvwxyz'
+        let _str: string = ''
+        str += str.toUpperCase();
+        str += '0123456789'
+        for (let i = 0; i < length; i++) {
+            let rand: number = Math.floor(Math.random() * str.length)
+            _str += str[rand];
+        }
+        return _str
+    }
+}
